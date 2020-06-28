55d9c3ac2f25ed423dfc98faa00ebe728480158e
Githubpoozas <po.oz.as@hotmail.com>
2020-06-28 19:29:48 +0700

    push for heroku

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectory = path.join(__dirname, '../public')
@@ -51,19 +52,22 @@ app.get('/weather', (req, res) => {
     })
   }
 
-  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
-    if (error) return res.send({ error })
-
-    forecast(latitude, longitude, (error, forecastData) => {
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
       if (error) return res.send({ error })
 
-      res.send({
-        forecast: forecastData,
-        location,
-        address: req.query.address,
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) return res.send({ error })
+
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        })
       })
-    })
-  })
+    }
+  )
 })
 
 app.get('/products', (req, res) => {
@@ -92,6 +96,6 @@ app.get('*', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000.')
+app.listen(port, () => {
+  console.log('Server is up on port ' + port)
 })
